ef4cf571fe9c339dd4716b2a64bf4f25952de8c2
Lionel Humbert <helloliohumb@gmail.com>
2021-05-01 17:31:49 +0200

    final commit view

--- lib/view.rb
+++ lib/view.rb
@@ -2,8 +2,13 @@ class View
 
   def create_gossip
     puts "Quel est ton nom ?"
+    puts ""
+    print "•    › ".green
     author = gets.chomp
-    puts "Quel est ton secret ?"
+    puts ""
+    puts "Ok #{author}, quel est ton secret ?"
+    puts ""
+    print "•    › ".green
     content = gets.chomp
     params = { author: author, content: content }
     return params
