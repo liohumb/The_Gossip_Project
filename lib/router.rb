c25a2fd0bc502ed1f577c354d01fc7171a39bd04
Lionel Humbert <helloliohumb@gmail.com>
2021-05-01 17:31:18 +0200

    final commit router

--- lib/router.rb
+++ lib/router.rb
@@ -9,27 +9,45 @@ class Router
 
 #rappelle-toi que l'on fait "Router.new.perform" dans app.rb => après initialize, on définit donc perform.
   def perform
-    puts "BIENVENUE DANS THE GOSSIP PROJECT"
+    system("clear")
+
+    puts "——————————————————————————————————————————————————"
+    puts "• /|\\/|\\ BIENVENUE DANS THE GOSSIP PROJECT/|\\/|\\ •"
+    puts "——————————————————————————————————————————————————"
 
     while true
 
       #on affiche le menu
-      puts "Tu veux faire quoi jeune mouss' ?"
-      puts "1. Je veux créer un gossip"
-      puts "4. Je veux quitter l'app"
+      puts ""
+      puts "Tu veux faire quoi jeune mouss' ?".center(50).green
+      puts ""
+      puts "• › " + "1".blue + " ‹ •  Je veux créer un gossip"
+      puts "• › " + "4".red + " ‹ •  Je veux quitter l'app"
+      puts ""
+      print "•    › ".green
       params = gets.chomp.to_i #on attend le choix de l'utilisateur
 
       case params #en fonction du choix
       when 1
-        puts "Tu as choisi de créer un gossip"
+        puts ""
+        puts "Tu as choisi de créer un gossip".center(50)
         @controller.create_gossip
 
       when 4
-        puts "À bientôt !"
+        puts "——————————————————————————————————————————————————"
+        puts "|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\"
+        puts "• \\|/\\/|\\/ Ton secret sera bien gardé ! \\|/\\/|\\/ •"
+        puts "• \\|/\\/|\\/         " + "À BIENTÔT !".green + "          \\|/\\/|\\/ •"
+        puts "|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\|/|\\"
+        puts "——————————————————————————————————————————————————"
+
+        puts ""
         break #Ce "break" permet de sortir de la boucle while. C'est même la seule façon d'en sortir.
 
       else
-        puts "Ce choix n'existe pas, merci de ressayer" #Si l'utilisateur saisit une entrée non prévue, il retourne au début du "while true".
+        puts ""
+        puts "Ce choix n'existe pas, merci de ressayer".center(50).red #Si l'utilisateur saisit une entrée non prévue, il retourne au début du "while true".
+        puts ""
         #C'est pour ça que la boucle est infinie: potentiellement, il peut se gourer jusqu'à la fin des temps :)
 
       end
